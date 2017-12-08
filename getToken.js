bfd41f8262d9bb27fb4c1e37885f398c1aad45b4
khunlingtan <kltan69@gmail.com>
2017-12-08 23:51:46 +0800

    setup user email & password

--- getToken.js
+++ getToken.js
@@ -1,11 +1,12 @@
-var request = require("request");
+var request = require('request');
+const USER = require('./config');
 
     var options = { method: 'POST',
       url: 'https://developers.onemap.sg/privateapi/auth/post/getToken',
       headers:
        { 'cache-control': 'no-cache',
          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-      formData: { email: 'hunter@velocity.com.sg', password: '98188729' } };
+      formData: { email: USER.email, password: USER.password } };
 
     request(options, function (error, response, body) {
       if (error) throw new Error(error);
